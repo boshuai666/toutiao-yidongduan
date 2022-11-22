ad145edb4e529ce219c346c34bed04dc87d867aa
boshuai666 <1990696239@qq.com>
2022-11-22 17:17:00 +0800

    项目初始化,引入vant组件库

--- src/App.vue
+++ src/App.vue
@@ -2,10 +2,17 @@
   <div id="app">
     <h1>黑马头条</h1>
     <router-view />
+    <i class="toutiao toutiao-shipin"></i>
+
+    <van-button type="primary">主要按钮</van-button>
+    <van-button type="info">信息按钮</van-button>
+    <van-button type="default">默认按钮</van-button>
+    <van-button type="warning">警告按钮</van-button>
+    <van-button type="danger">危险按钮</van-button>
   </div>
 </template>
-
 <script>
+//
 export default {
   name: 'App' // 可以在调试工具中找到这个组件,组件缓存需要组件name
 }
--- src/main.js
+++ src/main.js
@@ -7,6 +7,12 @@ import './styles/index.less'
 // 导入全局第三方vant组件库插件
 import './plugins/index'
 
+// 全局使用Vant组件库
+// 下载vant组件库 yarn add vant@2 并导入所有组件
+import Vant from 'vant'
+import 'vant/lib/index.css'
+Vue.use(Vant)// 区局注册Vant组件
+
 Vue.config.productionTip = false
 
 new Vue({
