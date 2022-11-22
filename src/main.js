ad63085a87dc955090a7d08d27b3d56698a90188
boshuai666 <1990696239@qq.com>
2022-11-22 09:38:57 +0800

    调整项目初始化结构

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入全局样式
+import './styles/index.less'
+// 导入全局第三方vant组件库插件
+import './plugins/index'
 
 Vue.config.productionTip = false
 
--- vue.config.js
+++ vue.config.js
@@ -1,3 +1,9 @@
+
 module.exports = {
-  lintOnSave: false
+  devServer: { // 自定义服务配置
+    port: 3000, // 端口
+    open: true, // 自动打开浏览器
+    host: 'localhost'
+  },
+  lintOnSave: false// 关闭eslint检查
 }
